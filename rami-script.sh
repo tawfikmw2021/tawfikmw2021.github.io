dc6ed2bc3a97fc7f2e1d4d60e9b61963331e21c0
Your Name <you@example.com>
2024-01-28 20:17:15 +0100

    add carte rami

---
+++ rami-script.sh
@@ -0,0 +1,3 @@
+sed -r -i "s/\"\/js\//\"\/rami-vue-dist\/js\//g" rami-vue-dist/index.html
+sed -r -i "s/\"\/img\//\"\/rami-vue-dist\/img\//g" rami-vue-dist/index.html
+sed -r -i "s/\"\/css\//\"\/rami-vue-dist\/css\//g" rami-vue-dist/index.html
